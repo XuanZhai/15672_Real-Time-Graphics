fc480f371569efcd2ac9b1893b338b4d007ed8e7
Xuan(James) Zhai <46932284+XuanZhai@users.noreply.github.com>
2024-01-21 22:21:36 -0500

    Add the texture image and the sampler. Need to combine them to represent the texture

--- Test_Program/TestProgram/main.cpp
+++ Test_Program/TestProgram/main.cpp
@@ -9,6 +9,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <windows.h>
 #include <iostream>
 #include <stdexcept>
@@ -286,6 +289,18 @@ private:
 
     /* Handles to the desciptor sets */
     std::vector<VkDescriptorSet> descriptorSets;
+
+    /* The image of the texture */
+    VkImage textureImage = VK_NULL_HANDLE;
+
+    /* The handle to the memory of the texture */
+    VkDeviceMemory textureImageMemory = VK_NULL_HANDLE;
+
+    /* The image view to access the texture image */
+    VkImageView textureImageView = VK_NULL_HANDLE;
+
+    /* The texture sampler instance to sample the texture image */
+    VkSampler textureSampler = VK_NULL_HANDLE;
 
 
     /* A struct of queue that will be submitted to Vulkan */
@@ -686,7 +701,7 @@ private:
         }
 
         return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-            deviceFeatures.geometryShader && indices.isComplete() && extensionsSupported && swapChainAdequate;
+            deviceFeatures.geometryShader && indices.isComplete() && extensionsSupported && swapChainAdequate && &deviceFeatures.samplerAnisotropy;
     }
 
 
@@ -767,6 +782,8 @@ private:
         }
 
         VkPhysicalDeviceFeatures deviceFeatures{};
+        deviceFeatures.samplerAnisotropy = VK_TRUE;     // Enable the anisotropy feature
+
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
         createInfo.pQueueCreateInfos = queueCreateInfos.data();
@@ -890,36 +907,44 @@ private:
 
 
     /**
+    * @brief Create a image view instance based on the image and its format
+    * @param[in] image: The image we are using.
+    * @param[in] format: The format of the image view.
+    */
+    VkImageView CreateImageView(VkImage image, VkFormat format)
+    {
+        VkImageViewCreateInfo viewInfo{};
+        viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        viewInfo.image = image;
+        viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        viewInfo.format = format;
+
+        /* The subresourceRange field describes what the image¡¯s purpose is and which part of the image should be accessed. */
+        viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        viewInfo.subresourceRange.baseMipLevel = 0;
+        viewInfo.subresourceRange.levelCount = 1;
+        viewInfo.subresourceRange.baseArrayLayer = 0;
+        viewInfo.subresourceRange.layerCount = 1;
+
+        /* Create the image view*/
+        VkImageView imageView;
+        if (vkCreateImageView(device, &viewInfo, nullptr, &imageView) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create image view!");
+        }
+
+        return imageView;
+    }
+
+
+    /**
     * @brief Create the Image Views that will handle the images in the swap chain.
     */
     void CreateImageViews() 
     {
         swapChainImageViews.resize(swapChainImages.size());
 
-        for (size_t i = 0; i < swapChainImages.size(); i++) {
-            VkImageViewCreateInfo createInfo{};
-            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-            createInfo.image = swapChainImages[i];
-            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-            createInfo.format = swapChainImageFormat;
-
-            /* Specify the color channel for the image view */
-            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-
-            /* The subresourceRange field describes what the image¡¯s purpose is and which part of the image should be accessed. */
-            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-            createInfo.subresourceRange.baseMipLevel = 0;
-            createInfo.subresourceRange.levelCount = 1;
-            createInfo.subresourceRange.baseArrayLayer = 0;
-            createInfo.subresourceRange.layerCount = 1;
-
-            /* Create the image view*/
-            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
-                throw std::runtime_error("failed to create image views!");
-            }
+        for (uint32_t i = 0; i < swapChainImages.size(); i++) {
+            swapChainImageViews[i] = CreateImageView(swapChainImages[i], swapChainImageFormat);
         }
     }
 
@@ -1274,14 +1299,9 @@ private:
     }
 
 
-    /**
-    * @brief Copy a vulkan buffer from sourse to destination.
-    * @param[in] srcBuffer: The source buffer.
-    * @param[in] dstBuffer: The destination buffer.
-    * @param[in] size: The copied buffer size.
-    */
-    void CopyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) 
-    {
+
+    VkCommandBuffer BeginSingleTimeCommands() {
+
         /* Allocate a small command buffer used to apply this transfer command */
         VkCommandBufferAllocateInfo allocInfo{};
         allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -1289,20 +1309,21 @@ private:
         allocInfo.commandPool = commandPool;
         allocInfo.commandBufferCount = 1;
 
+        /* Record the command buffer */
         VkCommandBuffer commandBuffer;
         vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
 
-        /* Record the command buffer */
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
         beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
         vkBeginCommandBuffer(commandBuffer, &beginInfo);
-            VkBufferCopy copyRegion{};
-            copyRegion.srcOffset = 0; // Optional
-            copyRegion.dstOffset = 0; // Optional
-            copyRegion.size = size;
-            vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+
+        return commandBuffer;
+    }
+
+
+    void EndSingleTimeCommands(VkCommandBuffer commandBuffer) {
         vkEndCommandBuffer(commandBuffer);
 
         /* Stop recording right after it's copied */
@@ -1316,6 +1337,24 @@ private:
 
         /* Free the command buffer */
         vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+    }
+
+
+    /**
+    * @brief Copy a vulkan buffer from sourse to destination.
+    * @param[in] srcBuffer: The source buffer.
+    * @param[in] dstBuffer: The destination buffer.
+    * @param[in] size: The copied buffer size.
+    */
+    void CopyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) 
+    {
+        VkCommandBuffer commandBuffer = BeginSingleTimeCommands();
+
+        VkBufferCopy copyRegion{};
+        copyRegion.size = size;
+        vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+
+        EndSingleTimeCommands(commandBuffer);
     }
 
 
@@ -1615,6 +1654,265 @@ private:
 
 
     /**
+    * @brief Create the image instance
+    * @param[in] width: The image's width.
+    * @param[in] height: The image's height.
+    * @param[in] format: Image's format
+    * @param[in] tiling: Image's tiling mode
+    * @param[in] usage: The image's usage type.
+    * @param[in] properties: The image's property.
+    * @param[out] image: The image instance.
+    * @param[out] imageMemory: The memory used to allocate the image.
+    */
+    void CreateImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory)
+    {
+        /* Set the info for the texture */
+        VkImageCreateInfo imageInfo{};
+        imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+        imageInfo.imageType = VK_IMAGE_TYPE_2D;                 // Texture is 2D
+        imageInfo.extent.width = static_cast<uint32_t>(width);
+        imageInfo.extent.height = static_cast<uint32_t>(height);
+        imageInfo.extent.depth = 1;
+        imageInfo.mipLevels = 1;                                // Mipmap level is 1
+        imageInfo.arrayLayers = 1;
+
+        imageInfo.format = format;
+        imageInfo.tiling = tiling;
+        imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        imageInfo.usage = usage;
+        imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;              // Used for multi-sampling
+        imageInfo.flags = 0; // Optional
+
+        if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create image!");
+        }
+
+        /* Allocate the memory for the image */
+        VkMemoryRequirements memRequirements;
+        vkGetImageMemoryRequirements(device, image, &memRequirements);
+
+        VkMemoryAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.allocationSize = memRequirements.size;
+        allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+
+        if (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate image memory!");
+        }
+
+        /* Bind the image with the memory */
+        vkBindImageMemory(device, image, imageMemory, 0);
+    }
+
+
+    /**
+    * @brief Copy a VkBuffer to a VkImage.
+    * @param[in] buffer: The buffer we are copying from.
+    * @param[in] image: The image we are copying to.
+    * @param[in] width: The image's width.
+    * @param[in] height: The image's height.
+    */
+    void CopyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height) {
+        VkCommandBuffer commandBuffer = BeginSingleTimeCommands();
+
+        VkBufferImageCopy region{};
+        region.bufferOffset = 0;
+        region.bufferRowLength = 0;
+        region.bufferImageHeight = 0;
+
+        region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.imageSubresource.mipLevel = 0;
+        region.imageSubresource.baseArrayLayer = 0;
+        region.imageSubresource.layerCount = 1;
+
+        region.imageOffset = { 0, 0, 0 };
+        region.imageExtent = {
+            width,
+            height,
+            1
+        };
+
+        vkCmdCopyBufferToImage(
+            commandBuffer,
+            buffer,
+            image,
+            VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+            1,
+            &region
+        );
+
+        EndSingleTimeCommands(commandBuffer);
+    }
+
+
+    /**
+    * @brief Transit the image's layout with a new layout using a pipline barrier.
+    * @param[in] image: The image that is affected and the specific part of the image.
+    * @param[in] format: The format of the image TODO!!!!!!!!!!!
+    * @param[in] oldLayout: The old layout we have.
+    * @param[in] newLayout: The new layout we determine.
+    */
+    void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout) {
+        VkCommandBuffer commandBuffer = BeginSingleTimeCommands();
+
+        VkImageMemoryBarrier barrier{};
+        barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+        barrier.oldLayout = oldLayout;
+        barrier.newLayout = newLayout;
+
+        /* Don't transfer the ownership of the queue family */
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+
+        barrier.image = image;
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        barrier.subresourceRange.baseMipLevel = 0;
+        barrier.subresourceRange.levelCount = 1;
+        barrier.subresourceRange.baseArrayLayer = 0;
+        barrier.subresourceRange.layerCount = 1;
+
+        /* Set the barrier access mask */
+        VkPipelineStageFlags sourceStage;
+        VkPipelineStageFlags destinationStage;
+
+        if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+            barrier.srcAccessMask = 0;
+            barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+            sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+            destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        }
+        else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+            barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+            barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+            sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+            destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+        }
+        else {
+            throw std::invalid_argument("unsupported layout transition!");
+        }
+
+        /* Submit the pipline barrier */
+        vkCmdPipelineBarrier(
+            commandBuffer,
+            sourceStage,       // Which pipeline stage the operations occur that should happen before the barrier
+            destinationStage,       // The pipeline stage in which operations will wait on the barrier.
+            0,
+            0, nullptr,
+            0, nullptr,
+            1, &barrier
+        );
+
+        EndSingleTimeCommands(commandBuffer);
+    }
+
+
+    /**
+    * @brief Create the texture image with a given texture.
+    */
+    void CreateTextureImage()
+    {
+        /* Load the texture image */
+        int texWidth, texHeight, texChannels;
+        stbi_uc* pixels = stbi_load("Textures/texture.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+        VkDeviceSize imageSize = texWidth * texHeight * 4;      // 4 means 4 bytes for pixel.
+
+        if (!pixels) {
+            throw std::runtime_error("failed to load texture image!");
+        }
+
+        /* Create a buffe to store the image data */
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
+        CreateBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+        /* Copy the image to the buffer */
+        void* data;
+        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+        memcpy(data, pixels, static_cast<size_t>(imageSize));
+        vkUnmapMemory(device, stagingBufferMemory);
+
+        stbi_image_free(pixels);
+
+        CreateImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+        
+        /* Copy the staging buffer to the texture image */
+        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+        CopyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+        
+        /* Change texture image's layout for the shade access */
+        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+    
+        /* Clear the stage buffer */
+        vkDestroyBuffer(device, stagingBuffer, nullptr);
+        vkFreeMemory(device, stagingBufferMemory, nullptr);
+    }
+
+
+    /**
+    * @brief Create the image view to access and present the texture image.
+    */
+    void CreateTextureImageView()
+    {
+        /* Create the texture image view */
+        textureImageView = CreateImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB);
+    }
+
+
+    /**
+    * @brief Create the texture sampler to access the texture.
+    * Apply filtering and transformations to compute the final color that is retrieved.
+    */
+    void CreateTextureSampler() 
+    {
+
+        VkSamplerCreateInfo samplerInfo{};
+        samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        /* How to interpolate texels that are magnified or minified */
+        /* Option: VK_FILTER_NEAREST or VK_FILTER_LINEAR */
+        samplerInfo.magFilter = VK_FILTER_LINEAR;
+        samplerInfo.minFilter = VK_FILTER_LINEAR;
+
+        /* VK_SAMPLER_ADDRESS_MODE_REPEAT: Repeat the texture when going beyond the image dimensions */
+        /* VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT: Repeat but mirror the image when beyond the dimensions */
+        /* VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE: Take the color of the edge closest to the coordinate */
+        /* VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE:  Like clamp but instead uses the edge opposite to the closest edge */
+        /* VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER: Return a solid color when beyond the dimension */
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+
+        /* Get the anisotrpy property from the physical device */
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(physicalDevice, &properties);
+        samplerInfo.anisotropyEnable = VK_TRUE;
+        samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+
+        samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+
+        /* If unnormalized, use coordinates within the [0, texWidth) and [0, texHeight) range. */
+        samplerInfo.unnormalizedCoordinates = VK_FALSE;
+
+        samplerInfo.compareEnable = VK_FALSE;
+        samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+
+        /* Set the mipmap info for the sampler */
+        samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        samplerInfo.mipLodBias = 0.0f;
+        samplerInfo.minLod = 0.0f;
+        samplerInfo.maxLod = 0.0f;
+        
+        /* Create the Sampler with the info */
+        if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create texture sampler!");
+        }
+    }
+
+
+    /**
     * @brief Create all the semaphores and fences used for sync updating the command buffers
     */
     void CreateSyncObjects()
@@ -1722,6 +2020,9 @@ private:
         CreateGraphicsPipeline();
         CreateFrameBuffers();
         CreateCommandPool();
+        CreateTextureImage();
+        CreateTextureImageView();
+        CreateTextureSampler();
         CreateVertexBuffer();
         CreateIndexBuffer();
         CreateUniformBuffers();
@@ -1904,6 +2205,11 @@ public:
         vkFreeMemory(device, indexBufferMemory, nullptr);
         vkDestroyBuffer(device, vertexBuffer, nullptr);
         vkFreeMemory(device, vertexBufferMemory, nullptr);
+
+        vkDestroySampler(device, textureSampler, nullptr);
+        vkDestroyImageView(device, textureImageView, nullptr);
+        vkDestroyImage(device, textureImage, nullptr);
+        vkFreeMemory(device, textureImageMemory, nullptr);
 
         vkDestroyDescriptorPool(device, descriptorPool, nullptr);
         vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
